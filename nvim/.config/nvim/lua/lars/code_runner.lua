817a224f52a0b8d1a0a7c46deee5b9c353e7cd4b
coesu <c>
2023-05-19 17:28:33 +0200

    something

---
+++ nvim/.config/nvim/lua/lars/code_runner.lua
@@ -0,0 +1,14 @@
+-- require("code_runner").setup({
+--     config = true,
+--     filetype = {
+--         python = "cd $dir && python3 $fileName"
+--     }
+-- })
+--
+-- vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+-- vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
