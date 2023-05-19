23421d62ce0380726f65fe9bd199ffbbc33fe2ee
coesu <c>
2023-05-19 17:32:48 +0200

    updated nvim

--- nvim/.config/nvim/lua/lars/remap.lua
+++ nvim/.config/nvim/lua/lars/remap.lua
@@ -73,3 +73,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 -- vim.keymap.set("n", "<leader><leader>", function()
 --     vim.cmd("so")
 -- end)
+--
+vim.keymap.set("n", "<leader>gs", "<cmd>LazyGit<CR>", {silent = true})
