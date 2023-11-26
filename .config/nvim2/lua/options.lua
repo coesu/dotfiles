e65ca9aa6b4814fba37b9917d56acaad186421f9
Lars Humpert <larshumpert@gmx.net>
2023-11-26 16:08:08 +0100

    updates

---
+++ .config/nvim2/lua/options.lua
@@ -0,0 +1,85 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.number = true
+-- vim.o.relativenumber = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.updatetime = 300
+
+vim.o.termguicolors = true
+
+vim.o.mouse = 'a'
+
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.textwidth=100
+vim.opt.spelllang="en"
+
+-- No swap file and undotree instead
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- KEYBINDINGS
+--
+
+-- simple saving and closing of panes
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>q", "<cmd>:close<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Navigate buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>")
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+
+-- Splits
+vim.keymap.set("n", "<leader>h", "<cmd>split<CR>")
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<CR>")
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- get into normal mode
+vim.keymap.set("i", "kj", "<Esc>")
+
+--format lsp
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", {silent = true})
+
+
+vim.keymap.set("n", "<leader>rht", "")
+vim.keymap.set("n", "<leader>rpd", "")
