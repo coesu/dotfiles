0486355f24b455fd5607dce91772b5a7692e0256
coesu <something>
2025-08-10 23:04:16 +0200

    updates

--- nvim/lua/lars/lazy/colors.lua
+++ nvim/lua/lars/lazy/colors.lua
@@ -1,10 +1,14 @@
 return {
-	"RRethy/base16-nvim",
-	config = function()
-		vim.cmd("colorscheme base16-gruvbox-dark-hard")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-	end,
+    "RRethy/base16-nvim",
+    config = function()
+        vim.cmd("colorscheme base16-gruvbox-dark-hard")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+        vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+        vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", fg = "#FFFFFF" })
+        vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
+    end,
 }
